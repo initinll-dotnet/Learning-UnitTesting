3b1383d73d20224447a0dcdc3f7e0e5720d94a2e
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-20 00:13:01 +0530

    Unit testing in the real world part 1 - UserServiceTests

---
+++ 4.RealWorld/test/Users.Api.Tests.Unit/Usings.cs
@@ -0,0 +1,10 @@
+global using Xunit;
+global using NSubstitute;
+global using FluentAssertions;
+global using Microsoft.Data.Sqlite;
+global using NSubstitute.ExceptionExtensions;
+
+global using Users.Api.Models;
+global using Users.Api.Logging;
+global using Users.Api.Services;
+global using Users.Api.Repositories;
