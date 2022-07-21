4783b5fe2c7f207c65d352c7878f5fc0e03ea49b
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-22 02:06:37 +0530

    Setting test execution timeouts

---
+++ 5.AdvancedTechniques/test/AdvancedTechniques.Tests.Unit/LongRunningTests.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedTechniques.Tests.Unit;
+
+public class LongRunningTests
+{
+    [Fact(Timeout = 2000)]
+    public async Task Slowest()
+    {
+        await Task.Delay(3000);
+    }
+}
